86b54246a5e60ad550a3d77f36b0181011261bda
Juan Pedro <juan.trionfini@tpv-sil.com>
2024-11-21 19:15:56 -0300

    add: i18n

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,12 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { SkyComponent } from '@app/components/sky/sky.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [SkyComponent, NgOptimizedImage],
+  imports: [SkyComponent, NgOptimizedImage, RouterLink],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  private router = inject(Router);
+
+  navigate(route: string) {
+    this.router.navigate([route])
+  }
+}
