74e982f4ef81b1bc9d66be6e5e9630326996f787
Juan Pedro <juan.trionfini@tpv-sil.com>
2024-11-23 12:11:41 -0300

    add: animations

--- src/app/pages/language/language.component.ts
+++ src/app/pages/language/language.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClickOutsideDirective } from '@app/commons/directive/click-outside-directive.directive';
 import { SkyComponent } from '@app/components/sky/sky.component';
+import anime from 'animejs/lib/anime.es.js';
 
 interface Language {
   code: string;
@@ -11,11 +13,12 @@ interface Language {
 @Component({
   selector: 'app-language',
   standalone: true,
-  imports: [SkyComponent, RouterLink],
+  imports: [SkyComponent, ClickOutsideDirective],
   templateUrl: './language.component.html',
   styleUrl: './language.component.scss',
 })
 export class LanguageComponent {
+  private _router = inject(Router)
   isOpen = false;
 
   languages: Language[] = [
@@ -30,5 +33,33 @@ export class LanguageComponent {
 
   closeDropdown(): void {
     this.isOpen = false;
+  }
+
+  ngOnInit() {
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    anime({
+      targets: '#animatedBox',
+      translateX: [-500, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      easing: 'linear',
+    });
+  }
+
+  selectLanguage() {
+    anime({
+      targets: '#animatedBox',
+      translateX: [0, 500],
+      opacity: [1, 0],
+      duration: 1000,
+      easing: 'linear',
+    });
+
+    setTimeout(() => {
+      this._router.navigate(['/select-mission']);
+    }, 1000);
   }
 }
